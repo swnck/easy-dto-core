2f1e892bb1dee6569dd89bb5ca25fbf6a26b4b29
jkvn <ceepkev77@gmail.com>
2024-03-24 18:28:45 +0100

    Migrate build system from Maven to Gradle
    The existing Maven configuration (pom.xml) has been deleted and replaced with new Gradle configuration files (build.gradle.kts, settings.gradle.kts, gradle-wrapper.properties).

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+import org.gradle.api.JavaVersion
+
+plugins {
+    id("java")
+    id("maven-publish")
+}
+
+group = "org.easybird"
+version = "1.0.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_21
+java.targetCompatibility = JavaVersion.VERSION_21
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.4")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testImplementation("org.mockito:mockito-core:3.11.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.11.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            this.groupId = project.group.toString()
+            this.artifactId = project.name
+            this.version = project.version.toString()
+        }
+    }
+
+    repositories {
+        maven {
+            name = "nexus"
+            val isSnapshot = project.version.toString().endsWith("-SNAPSHOT")
+            url = if (isSnapshot)
+                uri("https://nexus.synclyn.com/repository/maven-snapshots/")
+            else
+                uri("https://nexus.synclyn.com/repository/maven-releases/")
+            credentials {
+                username = System.getenv("NEXUS_USERNAME")
+                password = System.getenv("NEXUS_PASSWORD")
+            }
+        }
+    }
+}
